710f7925e0c47f0b9eb021ca1593e3c1ca7ed32c
E622014 <e6222014@sret.edu.in>
2025-06-05 08:45:49 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+Currency Converter
+Objective:
+Build a simple currency converter that converts INR to USD, EUR, or GBP.
+
+Requirements:
+
+Add:
+
+An input field to enter the amount in INR.
+
+A dropdown list to select the currency (USD, EUR, GBP).
+
+A Convert button.
+
+On clicking the button:
+
+Use fixed exchange rates (example):
+
+1 USD = 83 INR
+
+1 EUR = 89 INR
+
+1 GBP = 104 INR
+
+Convert the INR amount to the selected currency.
+
+Show the result below with the currency symbol.
+
+Add basic CSS to center-align the content and provide padding/margin.
